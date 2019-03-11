6bb503f53e96b0075bdc959576e310f9e8bf1135
Pavan Boorla <pushkarapavan@gmail.com>
2019-03-11 12:12:05 +0530

    Adding a program explaining initialization types

---
+++ 1_4_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int num1 = 5;
+   int num2(15);
+   int num3{25};
+   
+   std::cout<<"num1 :" << num1<<endl;
+   std::cout<<"num2 :" << num2<<endl;
+   std::cout<<"num3 :" << num3<<endl;
+   
+   
+   return 0;
+}
+
