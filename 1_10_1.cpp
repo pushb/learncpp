69944df3d116f8e701d0998402bc19fbbc91c81a
Pavan Boorla <pushkarapavan@gmail.com>
2019-03-12 09:58:56 +0530

    Adding another file for chapter 1

---
+++ 1_10_1.cpp
@@ -0,0 +1,15 @@
+/* These programs are written on top of a automatic template */
+
+#include <iostream>
+using namespace std;
+int main(){
+
+	int num1{0};
+	cout<<"Enter a number: ";
+	cin>>num1;
+
+	cout<<"Double the number is "<<num1*2<<endl;
+	
+	return 0;
+}
+
